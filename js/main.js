45682b7a65a339b16e730ccfd562b92e92ef651e
anekom <andreeiitaa100@gmail.com>
2020-04-16 15:42:03 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var sigientevalor = 0;
+
+var g2 = new JustGage({
+        id: 'g2',
+        value: 0,
+        min: 0,
+        max: 100,
+        symbol: '%',
+        pointer: true,
+        pointerOptions: {
+          toplength: -15,
+          bottomlength: 10,
+          bottomwidth: 12,
+          color: '#8e8e93',
+          stroke: '#ffffff',
+          stroke_width: 3,
+          stroke_linecap: 'round'
+        },
+        gaugeWidthScale: 0.6,
+        counter: true,
+        relativeGaugeSize: true
+      });
+
+  
---
+++ js/test.js
@@ -0,0 +1,11 @@
+var preguntas = [
+  "pregunta uno bla",
+  "pregunta dos bla bla",
+  "pregunta tres blabla bla"
+];
+
+var respuestas = [
+  ["a. respuesta a pregunta uno", "b. respuesta a pregunta uno", "c. respuesta a pregunta uno"],
+  ["a. respuesta a pregunta dos", "b. respuesta a pregunta dos", "c. respuesta a pregunta dos"],
+  ["a. respuesta a pregunta tres", "b. respuesta a pregunta tres", "c. respuesta a pregunta tres"]
+];
